87a601d0564e5e57cfdf267767be934ea20808fc
HonokaEndo <honoendo0130@gmail.com>
2024-09-10 00:59:47 +0900

    ランキングが評価が0になった時の動作を修正

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,14 +18,23 @@ class HomeController extends Controller
         $endOfWeek = Carbon::now()->endOfWeek();
 
         // 現在の週に投稿された上位5つの投稿を評価の平均点で取得し、同じ評価の場合は投稿日時でソート
+        // $topRecommends = Recommend::with('recommendReviews')
+        //     ->withAvg('recommendReviews as average_rating', 'star')
+        //     ->whereBetween('created_at', [$startOfWeek, $endOfWeek]) // 現在の週に投稿されたもののみ
+        //     ->orderByDesc('average_rating')
+        //     ->orderBy('created_at')
+        //     ->limit(5)
+        //     ->get();
+        
         $topRecommends = Recommend::with('recommendReviews')
             ->withAvg('recommendReviews as average_rating', 'star')
             ->whereBetween('created_at', [$startOfWeek, $endOfWeek]) // 現在の週に投稿されたもののみ
+            ->having('average_rating', '>', 0) // 評価が0より大きいもののみ表示
             ->orderByDesc('average_rating')
             ->orderBy('created_at')
             ->limit(5)
             ->get();
-            
+
         // ビューに $topRecommends 変数を渡す
         return view('home.homeindex', compact('topRecommends'));
     } 
